31d601e24b7de12905a10d59e9cae2bcd710ed42
Chris Couzens <ccouzens@gmail.com>
2024-01-13 21:29:44 +0000

    Implement the sidewinder algorithm

--- src/main.rs
+++ src/main.rs
@@ -236,7 +236,37 @@ fn generate_maze_with_binary_tree_algorithm(
     maze
 }
 
+fn generate_maze_with_sidewinder_algorithm(
+    rng: &mut impl rand::Rng,
+    width: usize,
+    height: usize,
+) -> Maze {
+    let mut maze = Maze::new(width, height).unwrap();
+
+    for x in 0..width - 1 {
+        maze.connect_pair(x, 0, x + 1, 0);
+    }
+
+    for y in 1..height {
+        let mut block_start = 0;
+        for x in 0..width {
+            let extend = x != width - 1 && rng.gen();
+            if extend {
+                maze.connect_pair(x, y, x + 1, y);
+            } else {
+                let up_cell = rng.gen_range(block_start..=x);
+                maze.connect_pair(up_cell, y, up_cell, y - 1);
+                block_start = x + 1;
+            }
+        }
+    }
+
+    maze
+}
+
 fn main() {
     let maze = generate_maze_with_binary_tree_algorithm(&mut rand::thread_rng(), 10, 10);
+    println!("{}\n", maze.as_block_printer());
+    let maze = generate_maze_with_sidewinder_algorithm(&mut rand::thread_rng(), 10, 10);
     println!("{}", maze.as_block_printer());
 }
