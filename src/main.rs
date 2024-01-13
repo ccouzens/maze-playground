19304649f74e0c11339448aa7d83782a86b6d7e6
Chris Couzens <ccouzens@gmail.com>
2024-01-13 16:49:49 +0000

    Support printing out a maze
    Next step, simple generator.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+/// A rectangular grid of cells with passageways or walls connecting or isolating them in the 4 cardinal directions
+///
+/// The top left corner cell has coordinates x=0, y=0.
+/// The top right corner cell has coordinates x=width-1, y=0.
+/// The bottom left corner cell has coordinates x=0, y=height-1.
+/// The bottom right corner cell has coordinates x=width-1, y=height-1.
 struct Maze {
     width: usize,
     height: usize,
@@ -50,8 +56,150 @@ impl Maze {
             walls: vec![true; wall_size],
         })
     }
+
+    fn as_block_printer(&self) -> BlockPrinter {
+        BlockPrinter { maze: self }
+    }
+
+    fn wall_index_between(
+        &self,
+        cell_a_x: usize,
+        cell_a_y: usize,
+        cell_b_x: usize,
+        cell_b_y: usize,
+    ) -> Option<usize> {
+        if cell_a_x >= self.width
+            || cell_b_x >= self.width
+            || cell_a_y >= self.height
+            || cell_b_y >= self.height
+        {
+            return None;
+        }
+        if cell_a_x == cell_b_x {
+            // Down from cell a
+            if cell_a_y + 1 == cell_b_y {
+                return Some(cell_a_y + cell_a_x * (self.height - 1));
+            }
+            // Up from cell a
+            if cell_a_y == cell_b_y + 1 {
+                return Some(cell_b_y + cell_a_x * (self.height - 1));
+            }
+        }
+
+        if cell_a_y == cell_b_y {
+            let horizontal_wall_offset = self.width * (self.height - 1);
+            // Right from cell a
+            if cell_a_x + 1 == cell_b_x {
+                return Some(horizontal_wall_offset + cell_a_y + cell_a_x * self.height);
+            }
+            // Left from cell a
+            if cell_a_x == cell_b_x + 1 {
+                return Some(horizontal_wall_offset + cell_a_y + cell_b_x * self.height);
+            }
+        }
+        None
+    }
+
+    fn is_connected_pair(
+        &self,
+        cell_a_x: usize,
+        cell_a_y: usize,
+        cell_b_x: usize,
+        cell_b_y: usize,
+    ) -> bool {
+        let wall_index = self.wall_index_between(cell_a_x, cell_a_y, cell_b_x, cell_b_y);
+        match wall_index {
+            None => false,
+            Some(i) => !self.walls[i],
+        }
+    }
+
+    fn connect_pair(
+        &mut self,
+        cell_a_x: usize,
+        cell_a_y: usize,
+        cell_b_x: usize,
+        cell_b_y: usize,
+    ) -> bool {
+        let wall_index = self.wall_index_between(cell_a_x, cell_a_y, cell_b_x, cell_b_y);
+        match wall_index {
+            None => false,
+            Some(i) => {
+                self.walls[i] = false;
+                true
+            }
+        }
+    }
+}
+
+struct BlockPrinter<'a> {
+    maze: &'a Maze,
+}
+
+impl std::fmt::Display for BlockPrinter<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for y in 0..self.maze.height {
+            for x in 0..self.maze.width {
+                write!(f, "█")?;
+                write!(
+                    f,
+                    "{}",
+                    if y == 0 || !self.maze.is_connected_pair(x, y - 1, x, y) {
+                        '█'
+                    } else {
+                        ' '
+                    }
+                )?;
+            }
+            writeln!(f, "█")?;
+            for x in 0..self.maze.width {
+                write!(
+                    f,
+                    "{}",
+                    if (x == 0 || !self.maze.is_connected_pair(x - 1, y, x, y))
+                        && !(x == 0 && y == self.maze.height - 1)
+                    {
+                        '█'
+                    } else {
+                        ' '
+                    }
+                )?;
+                write!(f, " ")?;
+            }
+            writeln!(f, "{}", if y == 0 { ' ' } else { '█' })?;
+        }
+        for _x in 0..self.maze.width {
+            write!(f, "██")?;
+        }
+        write!(f, "█")
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Maze;
+
+    #[test]
+    fn test_print_horizontal_zig() {
+        let expected = r"
+███████
+█   █  
+█ █ █ █
+  █   █
+███████"
+            .trim();
+
+        let mut maze = Maze::new(3, 2).unwrap();
+        maze.connect_pair(0, 1, 0, 0);
+        maze.connect_pair(0, 0, 1, 0);
+        maze.connect_pair(1, 0, 1, 1);
+        maze.connect_pair(1, 1, 2, 1);
+        maze.connect_pair(2, 1, 2, 0);
+        assert_eq!(expected, format!("{}", maze.as_block_printer()));
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut maze = Maze::new(20, 20).unwrap();
+    println!("{}", maze.as_block_printer());
 }
