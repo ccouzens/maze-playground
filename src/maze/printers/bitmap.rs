a625f4f4e572774dd3f1c204eba57dda01061158
Chris Couzens <ccouzens@gmail.com>
2024-02-08 08:19:38 +0000

    Let bitmap maze proportions be non 1:1

--- src/maze/printers/bitmap.rs
+++ src/maze/printers/bitmap.rs
@@ -2,6 +2,8 @@ use super::Maze;
 
 const WALL: [u8; 4] = [255, 255, 255, 255];
 const PASSAGE: [u8; 4] = [0, 0, 0, 0];
+const WALL_SIZE: usize = 1;
+const PASSAGE_SIZE: usize = 3;
 
 pub struct BitmapRenderer<'a> {
     pub maze: &'a Maze,
@@ -10,21 +12,27 @@ pub struct BitmapRenderer<'a> {
 impl BitmapRenderer<'_> {
     pub fn to_bitmap(&self) -> Vec<u8> {
         let mut bitmap = Vec::new();
+        let width = self.width();
+        let cell_size = WALL_SIZE + PASSAGE_SIZE;
 
         for y in 0..self.height() {
-            for x in 0..self.width() {
-                bitmap.extend(match (x % 2 == 0, y % 2 == 0) {
+            for x in 0..width {
+                let x_in_wall = x % cell_size < WALL_SIZE;
+                let y_in_wall = y % cell_size < WALL_SIZE;
+                let x_pos = x / cell_size;
+                let y_pos = y / cell_size;
+                bitmap.extend(match (x_in_wall, y_in_wall) {
                     (true, true) => WALL,
                     (false, false) => PASSAGE,
                     (false, true) => {
-                        if self.maze.displayed_as_horizontal_wall(x / 2, y / 2) {
+                        if self.maze.displayed_as_horizontal_wall(x_pos, y_pos) {
                             WALL
                         } else {
                             PASSAGE
                         }
                     }
                     (true, false) => {
-                        if self.maze.displayed_as_vertical_wall(x / 2, y / 2) {
+                        if self.maze.displayed_as_vertical_wall(x_pos, y_pos) {
                             WALL
                         } else {
                             PASSAGE
@@ -38,9 +46,9 @@ impl BitmapRenderer<'_> {
     }
 
     pub fn width(&self) -> usize {
-        self.maze.width * 2 + 1
+        self.maze.width * (WALL_SIZE + PASSAGE_SIZE) + WALL_SIZE
     }
     pub fn height(&self) -> usize {
-        self.maze.height * 2 + 1
+        self.maze.height * (WALL_SIZE + PASSAGE_SIZE) + WALL_SIZE
     }
 }
