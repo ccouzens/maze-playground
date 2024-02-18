a2f7970337a5d5e812a268055fb3cd3a137c5312
Chris Couzens <ccouzens@gmail.com>
2024-02-18 18:50:48 +0000

    Start a webgl2 rendering output
    So far it just does a pink square

--- wasm/www/src/index.ts
+++ wasm/www/src/index.ts
@@ -124,6 +124,75 @@ async function putMazeInMaskImage(computer: Computer, maze: Maze) {
   }
 }
 
+async function putMazeInWebgl(_computer: Computer, _maze: Maze) {
+  const canvas = document.querySelector<HTMLCanvasElement>("#webgl2");
+  const gl = canvas?.getContext("webgl2");
+  if (!gl) {
+    return;
+  }
+  gl.VERTEX_SHADER;
+
+  async function fetchAndCreateFunction(
+    gl: WebGL2RenderingContext,
+    type:
+      | WebGLRenderingContext["VERTEX_SHADER"]
+      | WebGLRenderingContext["FRAGMENT_SHADER"],
+    filename: string,
+  ) {
+    const response = await fetch(filename);
+    if (!response.ok) {
+      throw `Unexpected response ${response.status} ${response.statusText}`;
+    }
+    const source = await response.text();
+
+    const shader = gl.createShader(type)!;
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      return shader;
+    }
+
+    console.error(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    throw `Could not compile shader ${filename}`;
+  }
+
+  const [fragmentShader, vertexShader] = await Promise.all([
+    fetchAndCreateFunction(gl, gl.FRAGMENT_SHADER, "./maze.frag"),
+    fetchAndCreateFunction(gl, gl.VERTEX_SHADER, "./identity.vert"),
+  ]);
+
+  const program = gl.createProgram()!;
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+    throw `Could not compile program`;
+  }
+  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([-1, -1, -1, 1, 1, -1, 1, 1]),
+    gl.STATIC_DRAW,
+  );
+  const vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.useProgram(program);
+  gl.bindVertexArray(vao);
+
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+}
+
 export async function putMazeOnPage() {
   const computer = await initializeComputer();
   const maze = computer.new_maze(10, 10);
@@ -131,6 +200,7 @@ export async function putMazeOnPage() {
     putMazeInSvg(computer, maze);
     putMazeInPre(computer, maze);
     await putMazeInMaskImage(computer, maze);
+    await putMazeInWebgl(computer, maze);
   } finally {
     computer.free_maze(maze);
   }
