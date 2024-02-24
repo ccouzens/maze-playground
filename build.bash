eb4133fea37e236d0c700755220ae0b0c460b1f6
Chris Couzens <ccouzens@gmail.com>
2024-02-24 21:43:10 +0000

    Upload files in build script

--- build.bash
+++ build.bash
@@ -16,3 +16,29 @@ sed "s/-LATEST/-${VERSION}/g" < wasm/www/public/bundle-LATEST.js > "build/bundle
 cp wasm/www/public/computer-LATEST.wasm "build/computer-${VERSION}.wasm"
 cp wasm/www/public/frag-LATEST.glsl "build/frag-${VERSION}.glsl"
 cp wasm/www/public/vert-LATEST.glsl "build/vert-${VERSION}.glsl"
+
+curl -X POST --data-binary "@build/bundle-${VERSION}.js" \
+    -H "Authorization: Bearer ${GCP_TOKEN}" \
+    -H "Content-Type: application/javascript" \
+    "https://storage.googleapis.com/upload/storage/v1/b/maze-playground/o?uploadType=media&name=bundle-${VERSION}.js"
+
+curl -X POST --data-binary "@build/computer-${VERSION}.wasm" \
+    -H "Authorization: Bearer ${GCP_TOKEN}" \
+    -H "Content-Type: application/wasm" \
+    "https://storage.googleapis.com/upload/storage/v1/b/maze-playground/o?uploadType=media&name=wasm-${VERSION}.wasm"
+
+curl -X POST --data-binary "@build/frag-${VERSION}.glsl" \
+    -H "Authorization: Bearer ${GCP_TOKEN}" \
+    -H "Content-Type: text/plain" \
+    "https://storage.googleapis.com/upload/storage/v1/b/maze-playground/o?uploadType=media&name=frag-${VERSION}.glsl"
+
+curl -X POST --data-binary "@build/vert-${VERSION}.glsl" \
+    -H "Authorization: Bearer ${GCP_TOKEN}" \
+    -H "Content-Type: text/plain" \
+    "https://storage.googleapis.com/upload/storage/v1/b/maze-playground/o?uploadType=media&name=vert-${VERSION}.glsl"
+
+curl -X POST --data-binary "@build/index.html" \
+    -H "Authorization: Bearer ${GCP_TOKEN}" \
+    -H "Content-Type: text/html" \
+    "https://storage.googleapis.com/upload/storage/v1/b/maze-playground/o?uploadType=media&name=index.html"
+
