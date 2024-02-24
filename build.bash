cbef7ec4c9f59fb4dd2a33690a87e681e8042f23
Chris Couzens <ccouzens@gmail.com>
2024-02-24 21:28:36 +0000

    Add build script

---
+++ build.bash
@@ -0,0 +1,18 @@
+set -e
+
+# rustup target add wasm32-unknown-unknown
+# apt-get install binaryen
+# dnf install binaryen
+# brew install binaryen
+
+(cd wasm/www; ./build.bash)
+(cd wasm/www; pnpm install)
+(cd wasm/www; pnpm run build)
+
+VERSION="$(git rev-parse HEAD)"
+mkdir -p build
+sed "s/-LATEST/-${VERSION}/g" < wasm/www/public/index.html > build/index.html
+sed "s/-LATEST/-${VERSION}/g" < wasm/www/public/bundle-LATEST.js > "build/bundle-${VERSION}.js"
+cp wasm/www/public/computer-LATEST.wasm "build/computer-${VERSION}.wasm"
+cp wasm/www/public/frag-LATEST.glsl "build/frag-${VERSION}.glsl"
+cp wasm/www/public/vert-LATEST.glsl "build/vert-${VERSION}.glsl"
