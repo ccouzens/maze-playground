5ef2bbc6b68dec0f9585a359f6b8d3f2c6b8ab99
Chris Couzens <ccouzens@gmail.com>
2024-03-05 19:44:04 +0000

    Get a triangle on screen
    Thanks to
    https://webgpufundamentals.org/webgpu/lessons/webgpu-fundamentals.html

--- wasm/www/src/putMazeInWebGpu.ts
+++ wasm/www/src/putMazeInWebGpu.ts
@@ -1,10 +1,35 @@
 import { type Computer, type Maze } from "./index";
 
+function render(
+  context: GPUCanvasContext,
+  device: GPUDevice,
+  pipeline: GPURenderPipeline,
+) {
+  const encoder = device.createCommandEncoder({ label: "our encoder" });
+  const pass = encoder.beginRenderPass({
+    label: "our basic canvas renderPass",
+    colorAttachments: [
+      {
+        view: context.getCurrentTexture().createView(),
+        clearValue: [0.3, 0.3, 0.3, 1],
+        loadOp: "clear",
+        storeOp: "store",
+      },
+    ],
+  });
+  pass.setPipeline(pipeline);
+  pass.draw(3);
+  pass.end();
+
+  const commandBuffer = encoder.finish();
+  device.queue.submit([commandBuffer]);
+}
+
 export async function putMazeInWebGPU(_computer: Computer, _maze: Maze) {
   const device = await navigator.gpu
     ?.requestAdapter()
     .then((adapter) => adapter?.requestDevice());
-  if (!device) {
+  if (device === undefined) {
     console.log("WebGPU is not supported");
     return;
   }
@@ -15,8 +40,31 @@ export async function putMazeInWebGPU(_computer: Computer, _maze: Maze) {
     console.log("WebGPU is not supported");
     return;
   }
+  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
   context.configure({
     device,
-    format: navigator.gpu.getPreferredCanvasFormat(),
+    format: presentationFormat,
   });
+
+  const shaderCode = await fetch("./shader-LATEST.wgsl").then((r) => r.text());
+  const module = device.createShaderModule({
+    label: "shader from example",
+    code: shaderCode,
+  });
+
+  const pipeline = await device.createRenderPipelineAsync({
+    label: "example pipeline",
+    layout: "auto",
+    vertex: {
+      module,
+      entryPoint: "vs",
+    },
+    fragment: {
+      module,
+      entryPoint: "fs",
+      targets: [{ format: presentationFormat }],
+    },
+  });
+
+  render(context, device, pipeline);
 }
