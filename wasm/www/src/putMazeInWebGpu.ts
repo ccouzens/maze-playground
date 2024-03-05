5630a53708ded483f19e6e200d0cda81298eb1c5
Chris Couzens <ccouzens@gmail.com>
2024-03-05 08:11:43 +0000

    Avoid some temporary variables

--- wasm/www/src/putMazeInWebGpu.ts
+++ wasm/www/src/putMazeInWebGpu.ts
@@ -1,14 +1,16 @@
 import { type Computer, type Maze } from "./index";
 
 export async function putMazeInWebGPU(_computer: Computer, _maze: Maze) {
-  const adapter = await navigator.gpu?.requestAdapter();
-  const device = await adapter?.requestDevice();
+  const device = await navigator.gpu
+    ?.requestAdapter()
+    .then((adapter) => adapter?.requestDevice());
   if (!device) {
     console.log("WebGPU is not supported");
     return;
   }
-  const canvas = document.querySelector<HTMLCanvasElement>("#webgpu")!;
-  const context = canvas.getContext("webgpu");
+  const context = document
+    .querySelector<HTMLCanvasElement>("#webgpu")
+    ?.getContext("webgpu");
   if (!context) {
     console.log("WebGPU is not supported");
     return;
