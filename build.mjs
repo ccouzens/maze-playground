78a6510d8fcb45dabe1017581e10785f03455bc6
Chris Couzens <ccouzens@gmail.com>
2024-03-23 00:19:39 +0000

    Support dev server

--- build.mjs
+++ build.mjs
@@ -3,71 +3,100 @@ import * as esbuild from "esbuild";
 import * as fs from "node:fs/promises";
 import { spawn } from "node:child_process";
 
-console.log("cargo build --target=wasm32-unknown-unknown --release");
-const cargo = spawn(
-  "cargo",
-  ["build", "--target=wasm32-unknown-unknown", "--release"],
-  {
-    cwd: "computer",
-  },
-);
+async function compileRust() {
+  console.log("cargo build --target=wasm32-unknown-unknown --release");
+  const cargo = spawn(
+    "cargo",
+    ["build", "--target=wasm32-unknown-unknown", "--release"],
+    {
+      cwd: "computer",
+    },
+  );
 
-cargo.stdout.on("data", (d) => {
-  console.log(d.toString());
-});
-cargo.stderr.on("data", (d) => {
-  console.error(d.toString());
-});
-
-await new Promise((resolve, reject) => {
-  cargo.on("close", (code) => {
-    if (code === 0) {
-      resolve();
-    } else {
-      reject(`cargo exited with status ${code}`);
-    }
+  cargo.stdout.on("data", (d) => {
+    console.log(d.toString());
   });
-});
+  cargo.stderr.on("data", (d) => {
+    console.error(d.toString());
+  });
 
-console.log("rm -r ./build/");
-await fs.rm("./build/", {
-  recursive: true,
-  force: true,
-});
+  await new Promise((resolve, reject) => {
+    cargo.on("close", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(`cargo exited with status ${code}`);
+      }
+    });
+  });
+}
 
-const buildOutput = await esbuild.build({
+const esbuildOptions = {
   entryPoints: ["src/script.ts", "public/style.css"],
-  bundle: true,
   loader: {
     ".wasm": "file",
     ".glsl": "file",
     ".wgsl": "file",
   },
-  assetNames: "../build/[name]-[hash]",
-  entryNames: "[name]-[hash]",
-  minify: true,
-  sourcemap: true,
-  outdir: "./build",
   logLevel: "info",
-  metafile: true,
-});
+  bundle: true,
+  sourcemap: true,
+};
 
-const outputs = Object.keys(buildOutput.metafile.outputs).map((o) => {
-  const m = o.match(
-    /^build\/(?<name>[^-]+)(?<hash>-[A-Z0-9]{8})(?<ext>\..+)/,
-  ).groups;
-  return [`${m.name}${m.ext}`, `${m.name}${m.hash}${m.ext}`];
-});
+async function build() {
+  console.log("rm -r ./build/");
+  await fs.rm("./build/", {
+    recursive: true,
+    force: true,
+  });
 
-console.log(outputs);
+  const buildOutput = await esbuild.build({
+    ...esbuildOptions,
+    assetNames: "[name]-[hash]",
+    entryNames: "[name]-[hash]",
+    minify: true,
+    outdir: "./build",
+    metafile: true,
+  });
 
-console.log("cat public/index.html");
-let html = await fs.readFile("public/index.html", {
-  encoding: "utf8",
-});
-for (const [orig, hashed] of outputs) {
-  html = html.replace(orig, hashed);
+  const outputs = Object.keys(buildOutput.metafile.outputs).map((o) => {
+    const m = o.match(
+      /^build\/(?<name>[^-]+)(?<hash>-[A-Z0-9]{8})(?<ext>\..+)/,
+    ).groups;
+    return [`${m.name}${m.ext}`, `${m.name}${m.hash}${m.ext}`];
+  });
+
+  console.log(outputs);
+
+  console.log("cat public/index.html");
+  let html = await fs.readFile("public/index.html", {
+    encoding: "utf8",
+  });
+  for (const [orig, hashed] of outputs) {
+    html = html.replace(orig, hashed);
+  }
+
+  console.log("cat > build/index.html");
+  await fs.writeFile("build/index.html", html);
 }
 
-console.log("cat > build/index.html");
-await fs.writeFile("build/index.html", html);
+async function serve() {
+  const ctx = await esbuild.context({
+    ...esbuildOptions,
+    entryNames: "[name]",
+    assetNames: "[name]",
+    outdir: "public/",
+    write: false,
+  });
+
+  await ctx.serve({
+    servedir: "public",
+  });
+}
+
+await compileRust();
+if (process.argv[2] === "serve") {
+  await serve();
+} else {
+  await build();
+}
