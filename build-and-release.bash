22eccc6fd0bb8de90814f10c2c657f0065cc560a
Chris Couzens <ccouzens@gmail.com>
2024-04-05 20:50:52 +0000

    Move -Z below cp
    https://cloud.google.com/storage/docs/gsutil/commands/cp
    
    I'm currently getting the message
    
    ```
    CommandException: option -Z not recognized
    ```

--- build-and-release.bash
+++ build-and-release.bash
@@ -7,61 +7,61 @@ set -e
 VERSION="$(git rev-parse HEAD)"
 
 gsutil \
+  cp \
   -Z \
   -h "Content-Type:text/plain" \
-  cp \
   "build/frag-*.glsl" \
   "build/vert-*.glsl" \
   "build/shader-*.wgsl" \
   gs://maze-playground/
 
 gsutil \
+  cp \
   -Z \
   -h "Content-Type:application/wasm" \
-  cp \
   "build/*.wasm" \
   gs://maze-playground/
 
 gsutil \
+  cp \
   -Z \
   -h "Content-Type:text/javascript" \
-  cp \
   "build/*.js" \
   gs://maze-playground/
   
 gsutil \
+  cp \
   -Z \
   -h "Content-Type:text/css" \
-  cp \
   "build/*.css" \
   gs://maze-playground/
   
 gsutil \
+  cp \
   -Z \
   -h "Content-Type:application/json" \
-  cp \
   "build/*.js.map" \
   "build/*.css.map" \
   gs://maze-playground/
 
 gsutil \
+  cp \
   -Z \
   -h "Content-Type:text/html" \
-  cp \
   "build/rendering-playground.html" \
   "build/game.html" \
   gs://maze-playground/
 
 gsutil \
+  cp \
   -Z \
   -h "Content-Type:text/html" \
-  cp \
   "build/rendering-playground.html" \
   "gs://maze-playground/rendering-playground-${VERSION}.html"
 
 gsutil \
+  cp \
   -Z \
   -h "Content-Type:text/html" \
-  cp \
   "build/game.html" \
   "gs://maze-playground/game-${VERSION}.html"
