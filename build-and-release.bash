eec208f8d77ddc5bc03b92a6c4f684187f678e52
Chris Couzens <ccouzens@gmail.com>
2024-04-05 21:21:03 +0000

    Move cp back below
    from the time this was working

--- build-and-release.bash
+++ build-and-release.bash
@@ -11,28 +11,32 @@ cp build/rendering-playground.html "build/rendering-playground-${VERSION}.html"
 
 ls -- build
 
-gsutil cp \
+gsutil \
   -h "Content-Type:text/plain" \
   -Z \
+  cp \
   build/*.glsl \
   build/*.wgsl \
   gs://maze-playground/
 
-gsutil cp \
+gsutil \
   -h "Content-Type:application/wasm" \
   -Z \
+  cp \
   build/*.wasm \
   gs://maze-playground/
   
-gsutil cp \
+gsutil \
   -h "Content-Type:application/json" \
   -Z \
+  cp \
   build/*.js.map \
   build/*.css.map \
   gs://maze-playground/
 
-gsutil cp \
+gsutil \
   -Z \
+  cp \
   build/*.js \
   build/*.css \
   build/*.html \
