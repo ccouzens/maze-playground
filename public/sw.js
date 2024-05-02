fc98c316ad28e274c9118636a5c813d47f633da7
Chris Couzens <ccouzens@gmail.com>
2024-05-02 21:10:42 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
-  console.log("fetch requested", url);
+  console.log("fetch requested", url.href);
   event.respondWith(fetch(url));
 });
