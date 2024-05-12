46ee86af08fed254dcf3dd88d0a46a13d8127497
Chris Couzens <ccouzens@gmail.com>
2024-05-12 09:24:11 +0100

    Start adding vite
    It certainly seems like it may have been the better initial option

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,10 @@
 import { initialiseApp } from "./game/app";
+import sw from "./sw.js?url";
 
 initialiseApp(window).catch((reason) => console.error(reason));
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js").then(
+  navigator.serviceWorker.register(sw).then(
     (r) => console.log("Service worker registered", r),
     (e) => console.log("Service worker failed to register", e),
   );
