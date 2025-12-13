22da98aa27098f35941185e691b452a439c82489
Chris Couzens <ccouzens@gmail.com>
2025-12-13 20:25:14 +0000

    Swap dialogs to open with Invoker Commands
    Freshly supported by Safari, and with existing support from Chrome
    and Firefox.

--- src/game/app.ts
+++ src/game/app.ts
@@ -14,24 +14,15 @@ interface WindowType {
     querySelector: Window["document"]["querySelector"];
     querySelectorAll: Window["document"]["querySelectorAll"];
   };
-  history: {
-    back: Window["history"]["back"];
-    pushState: Window["history"]["pushState"];
-    replaceState: Window["history"]["replaceState"];
-    state: Window["history"]["state"];
-  };
   navigator: {
     share: Window["navigator"]["share"];
   };
   location: {
-    hash: Window["location"]["hash"];
     href: Window["location"]["href"];
   };
 }
 
 interface App {
-  optionsDialog: HTMLDialogElement;
-  linksDialog: HTMLDialogElement;
   winDialog: HTMLDialogElement;
   wallsSvgPath: SVGPathElement;
   routeSvgPath: SVGPathElement;
@@ -43,41 +34,6 @@ interface App {
   computer: Computer;
   maze: Maze | null;
   window: WindowType;
-}
-
-function navigate(app: App, key: string, pushState = true) {
-  let dialog: HTMLDialogElement;
-  if (key === "#options") {
-    dialog = app.optionsDialog;
-  } else if (key === "#links") {
-    dialog = app.linksDialog;
-  } else if (key === "#win") {
-    if (!app.maze || !app.computer.maze_is_solved(app.maze)) {
-      return;
-    }
-    dialog = app.winDialog;
-  } else {
-    return;
-  }
-  dialog.showModal();
-  if (pushState) {
-    app.window.history.pushState(key, "", key);
-  }
-  dialog.addEventListener(
-    "close",
-    function dialogClose() {
-      if (app.window.history.state === key) {
-        app.window.history.back();
-      } else {
-        app.window.history.replaceState(
-          null,
-          "",
-          app.window.location.href.replace(/#.+/, ""),
-        );
-      }
-    },
-    { once: true },
-  );
 }
 
 function newMaze(app: App): void {
@@ -117,43 +73,19 @@ function newMaze(app: App): void {
   renderAfterMove(app);
 }
 
-function popstateHandlerFactory(
-  app: App,
-): (this: Window, ev: WindowEventMap["popstate"]) => void {
-  return function popstateHandler({ state }) {
-    if (state === null) {
-      for (const dialog of app.window.document.querySelectorAll<HTMLDialogElement>(
-        "dialog[open]",
-      )) {
-        dialog.close();
-      }
-    } else if (typeof state === "string") {
-      navigate(app, state, false);
-    }
-  };
-}
-
 function clickHandlerFactory(
   app: App,
 ): (this: HTMLElement, ev: HTMLElementEventMap["click"]) => void {
   return function clickHandler(ev) {
     if (ev.target instanceof HTMLDialogElement) {
       ev.target.close();
-    } else if (ev.target instanceof HTMLAnchorElement) {
-      const hrefAttr = ev.target.getAttribute("href");
-      if (hrefAttr?.startsWith("#")) {
-        navigate(app, hrefAttr);
-        ev.preventDefault();
-      }
     } else if (ev.target instanceof HTMLButtonElement) {
       const value = ev.target.value;
       if (value === "new") {
         newMaze(app);
       } else if (value === "share") {
-        const url = new URL(app.window.location.href);
-        url.hash = "";
         app.window.navigator.share({
-          url: url.href,
+          url: app.window.location.href,
           title: "Maze puzzle",
         });
       }
@@ -230,8 +162,8 @@ function renderAfterMove(app: App) {
     );
 
     const won = app.computer.maze_is_solved(app.maze);
-    if (won && !app.root.classList.contains("win")) {
-      navigate(app, "#win", true);
+    if (won) {
+      app.winDialog.showModal();
     }
     app.root.classList.toggle("win", won);
   }
@@ -245,10 +177,6 @@ function lookupElements(
     "#optionsDialog form",
   )!;
   return {
-    optionsDialog:
-      window.document.querySelector<HTMLDialogElement>("#optionsDialog")!,
-    linksDialog:
-      window.document.querySelector<HTMLDialogElement>("#linksDialog")!,
     winDialog: window.document.querySelector<HTMLDialogElement>("#winDialog")!,
     wallsSvgPath: mazeSvg.querySelector<SVGPathElement>("#walls")!,
     routeSvgPath: mazeSvg.querySelector<SVGPathElement>("#route")!,
@@ -275,19 +203,13 @@ export async function initialiseApp(window: WindowType) {
   };
   const clickHandler = clickHandlerFactory(app);
   const optionsInputHandler = () => newMaze(app);
-  const popstateHandler = popstateHandlerFactory(app);
   const pointerMoveHandler = pointerMoveHandlerFactory(app);
   const keyDownHandler = keyDownHanderFactory(app);
 
   app.window.document.body.addEventListener("click", clickHandler);
   app.optionsForm.addEventListener("input", optionsInputHandler);
-  app.window.addEventListener("popstate", popstateHandler);
   app.mazeSvg.addEventListener("pointermove", pointerMoveHandler);
   app.window.addEventListener("keydown", keyDownHandler);
-
-  if (window.location.hash) {
-    navigate(app, app.window.location.hash, false);
-  }
 
   app.root.classList.toggle("supports-share", "share" in window.navigator);
 
